3dda479abd6ce8d23c8037785b44dabec9395fa4
Tmrvarun <er.varuntomar@gmail.com>
2026-07-17 14:59:23 +0530

    Update basic.ts

--- basic.ts
+++ basic.ts
@@ -6,7 +6,7 @@ console.log('welcome to typescript');
 //Check the path of node js> it is already available
 //Create folder in any drive > Load the folder in visual studio
 //In VS create new folder > create a file in it 
-// Download the tyscript compiler by following command in cmd "npm install -g typescript"
+// Download the typescript compiler by following command in cmd "npm install -g typescript"
 //To execute code first convert code to js file by "tsc file path.ts" , then "node file path.ts"
 // if during compilation, error occur "Running scripts is disable", then by command "Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned",is is removed
 // install typescript executor by command "npm install -g tsx" if you want to skip the ts conversion to js
